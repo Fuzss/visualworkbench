71d4e0c0cf8db618443b828aa0f589e91d401e1d
Fuzss <28218241+Fuzss@users.noreply.github.com>
2024-08-16 21:19:42 +0200

    fix NullPointerException in menu

--- 1.21/Common/src/main/java/fuzs/visualworkbench/world/inventory/VisualCraftingMenu.java
+++ 1.21/Common/src/main/java/fuzs/visualworkbench/world/inventory/VisualCraftingMenu.java
@@ -4,6 +4,7 @@ import fuzs.visualworkbench.init.ModRegistry;
 import fuzs.visualworkbench.world.level.block.entity.CraftingTableBlockEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.Container;
+import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.*;
@@ -11,18 +12,18 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 
 public class VisualCraftingMenu extends CraftingMenu implements ContainerListener {
-    private final Container container;
+    private final Container blockEntity;
 
     public VisualCraftingMenu(int id, Inventory inventory) {
         super(id, inventory);
-        this.container = null;
+        this.blockEntity = new SimpleContainer();
     }
 
     public VisualCraftingMenu(int id, Inventory inventory, CraftingTableBlockEntity blockEntity, ContainerLevelAccess access) {
         super(id, inventory, access);
         ((TransientCraftingContainer) this.craftSlots).items = blockEntity.getItems();
         this.resultSlots.itemStacks = blockEntity.getResultItems();
-        this.container = blockEntity;
+        this.blockEntity = blockEntity;
         this.addSlotListener(this);
         // always update recipe output when opening menu, otherwise could be missing or outdated if the block entity didn't save it correctly
         this.slotsChanged(this.craftSlots);
@@ -38,7 +39,7 @@ public class VisualCraftingMenu extends CraftingMenu implements ContainerListene
         super.slotsChanged(container);
         if (container == this.craftSlots || container == this.resultSlots) {
             this.access.execute((Level level, BlockPos blockPos) -> {
-                this.container.setChanged();
+                this.blockEntity.setChanged();
             });
         }
     }
@@ -56,13 +57,17 @@ public class VisualCraftingMenu extends CraftingMenu implements ContainerListene
     public boolean stillValid(Player player) {
         return this.access.evaluate((Level level, BlockPos blockPos) -> {
             // craft slots are extended to forward this to the block entity, normally in vanilla this would always return true
-            return this.container.stillValid(player);
+            return this.blockEntity.stillValid(player);
         }, true);
     }
 
     @Override
     public void slotChanged(AbstractContainerMenu containerToSend, int dataSlotIndex, ItemStack itemStack) {
-        if (dataSlotIndex >= 0 && dataSlotIndex < this.getSize()) this.container.setChanged();
+        this.access.execute((Level level, BlockPos blockPos) -> {
+            if (dataSlotIndex >= 0 && dataSlotIndex < this.getSize()) {
+                this.blockEntity.setChanged();
+            }
+        });
     }
 
     @Override
